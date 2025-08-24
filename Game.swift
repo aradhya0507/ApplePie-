d6c87b130b898ed24ead62fb96597f802b4f2cd4
aradhya0507 <aradhya.bhagwat@gmail.com>
2025-08-24 11:54:45 +0530

    Added project files.

---
+++ Game.swift
@@ -0,0 +1,37 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by SDC-USER on 18/08/25.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+        
+    }
+    
+    var formattedWord: String {
+        var guessedWord: String = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            }
+            else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
+
+
